7439a52df2e9d502e6fc8cd178668878951b9172
rk5634 <rajiv0099c@gmail.com>
2025-06-12 09:06:07 +0530

    Edited signup/route.ts

--- src/app/api/auth/signup/route.ts
+++ src/app/api/auth/signup/route.ts
@@ -5,6 +5,7 @@ export async function POST(req: NextRequest) {
     let requestBody;
     try {
         requestBody = await req.json();
+        console.log('Request body:', requestBody);
     } catch (error: unknown) {
         console.error('Error during signup proxy:', error);
         return NextResponse.json(
@@ -12,5 +13,6 @@ export async function POST(req: NextRequest) {
             { status: 500 }
         );
     };
+
   return NextResponse.json({ message: 'Signup successful' });
 }
