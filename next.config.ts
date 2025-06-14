5af60225d4411bf3d969d3de1017536b81defac4
rk5634 <rajiv0099c@gmail.com>
2025-06-14 19:22:59 +0530

    commented out something in next.congig.ts file

--- next.config.ts
+++ next.config.ts
@@ -2,7 +2,7 @@
 const isProd = process.env.NODE_ENV === 'production';
 
 const nextConfig = {
-  output: 'export',
+  // output: 'export',
   basePath: isProd ? '' : '',
   assetPrefix: isProd ? '' : '',
 };
